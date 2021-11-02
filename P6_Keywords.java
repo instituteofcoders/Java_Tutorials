3afd56fa762955ad2552fa590caaadf84b98558d
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 15:48:17 +0530

    Use of Keywords and Identifiers

---
+++ P6_Keywords.java
@@ -0,0 +1,15 @@
+//Keywords and Identifiers (Variable,Class, Method)
+package javatutorial;
+
+
+public class Keywords
+{
+    public static void main(String[] args)
+    {
+        int a_1=50;
+        System.out.print("Hello World");
+        //public class sum();
+        
+    }
+    
+}
