306f1c2a84177d76d8e88ff3c029814fd09cff76
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 15:47:26 +0530

    Use of Escape Sequence

---
+++ P5_Escape.java
@@ -0,0 +1,14 @@
+
+package javatutorial;
+
+
+public class Escape
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+        
+    }
+    
+}
