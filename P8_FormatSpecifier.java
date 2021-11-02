4ec9d6387cc063498ff0a3bd0ceacfa4c30f5ed3
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 15:49:10 +0530

    Use of Format Specifier

---
+++ P8_FormatSpecifier.java
@@ -0,0 +1,30 @@
+
+package javatutorial;
+
+
+public class P8FormatSpecifier
+{
+    public static void main(String[] args)
+    {
+        byte b=5;
+        short s=100;
+        int i=1000;
+        long l=546987;
+        float f=12.54f;
+        double d=123546.126;
+        boolean bo=true;
+        char c='A';
+         
+            System.out.printf("b=%d\n",b);
+            System.out.printf("s=%d\n",s);
+            System.out.printf("i=%d\n",i);
+            System.out.printf("l=%d\n",l);
+            System.out.printf("f=%f\n",f);
+            System.out.printf("d=%f\n",d);
+            System.out.printf("bo=%b\n",bo);
+            System.out.printf("c=%c",c);
+
+
+    }
+    
+}
