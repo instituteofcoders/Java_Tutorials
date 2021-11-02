0ad7f0ab1802e5b56a8673fcbe7db2c5db470b3e
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 15:48:48 +0530

    Use of Data Types

---
+++ P7_Datatypes.java
@@ -0,0 +1,30 @@
+
+package javatutorial;
+
+
+public class P7Datatypes
+{
+    public static void main(String[] args)
+    {
+        byte b=5;
+        short s=100;
+        int i=1000;
+        long l=546987;
+        float f=12.54f;
+        double d=123546.126;
+        boolean bo=true;
+        char c='A';
+         
+            System.out.println("b="+b);
+            System.out.println("s="+s);
+            System.out.println("i="+i);
+            System.out.println("l="+l);
+            System.out.println("f="+f);
+            System.out.println("d="+d);
+            System.out.println("bo="+bo);
+            System.out.println("c="+c);
+
+
+    }
+    
+}
