982015d8d0ce9c7a8e1b6735901ab1e7d56b3131
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-02 15:46:40 +0530

    Use of Comment

---
+++ P4_Comment.java
@@ -0,0 +1,16 @@
+
+/*Use
+of
+Comment*/
+package javatutorial;
+
+
+public class Comment
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World");//This is a library method
+        
+    }
+    
+}
