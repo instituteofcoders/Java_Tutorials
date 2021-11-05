8f2ff83f4adfb92e7e46bcd07fb55227e5f1e2e3
instituteofcoders <72811704+instituteofcoders@users.noreply.github.com>
2021-11-05 09:09:06 +0530

    Getting User Input | String | next( ) | nextLine( )

---
+++ P11_String.java
@@ -0,0 +1,18 @@
+package javatutorial;
+
+import java.util.Scanner;
+
+
+public class P11_String
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String day;
+        System.out.println("Today is which day?");
+        day=sc.nextLine();
+        System.out.println("Today is:"+day);
+
+    }
+    
+}
